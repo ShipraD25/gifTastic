d3b5275267e048e633538602ca828f73327764b9
Shipra <drshipradyundi@gmail.com>
2019-11-03 05:31:44 -0800

    adding some styling to button area

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
         });
     });
     $(document).on("click", ".gif", function() {
-        //event.preventDefault();
+        event.preventDefault();
         // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
         var state = $(this).attr("data-state");
         // If the clicked image's state is still, update its src attribute to what its data-animate value is.
